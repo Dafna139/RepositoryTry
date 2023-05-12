9577b33c7485b7ad2d23d12d14285fe4a465cd62
Dafna139 <133394559+Dafna139@users.noreply.github.com>
2023-05-12 21:38:15 +0300

    Update TryNew.asmx.cs

--- TryNew.asmx.cs
+++ TryNew.asmx.cs
@@ -16,18 +16,46 @@ namespace WebServices
     // [System.Web.Script.Services.ScriptService]
     public class TryNew : System.Web.Services.WebService
     {
-        static string myEmail = "";
+        [WebMethod]
+        public bool IsValidEmail(string email)
+        {
+            try
+            {
+                // Normalize the domain
+                email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                                      RegexOptions.None, TimeSpan.FromMilliseconds(200));
 
-        private static string MyEmail { get => myEmail; set => myEmail = value; }
-        [WebMethod]
-        public string GetEmail()
-        {
-            return MyEmail;
-        }
-        [WebMethod]
-        public void SetEmail(string email)
-        {
-            myEmail = email;
+                // Examines the domain part of the email and normalizes it.
+                string DomainMapper(Match match)
+                {
+                    // Use IdnMapping class to convert Unicode domain names.
+                    var idn = new IdnMapping();
+
+                    // Pull out and process domain name (throws ArgumentException on invalid)
+                    string domainName = idn.GetAscii(match.Groups[2].Value);
+
+                    return match.Groups[1].Value + domainName;
+                }
+            }
+            catch (RegexMatchTimeoutException e)
+            {
+                return false;
+            }
+            catch (ArgumentException e)
+            {
+                return false;
+            }
+
+            try
+            {
+                return Regex.IsMatch(email,
+                    @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                    RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
         }
     }
 }
